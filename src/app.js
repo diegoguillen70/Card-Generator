feec6b222e6cba200350f242e6ee1c967a6e89f1
diegoguillen70 <diegoguillen70@gmail.com>
2023-08-09 02:03:21 +0000

    added change size

--- src/app.js
+++ src/app.js
@@ -7,43 +7,64 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let symbol = ["♠", "♣", "♥", "♦"];
   generateSymbol();
   let timer = setInterval(() => {
     generateSymbol();
   }, 10000);
-  let buttons = document.getElementsByTagName("button");
-  buttons[0].addEventListener("click", e => {
-    generateSymbol();
-    clearInterval(timer);
-  });
-};
-let symbol = ["♠", "♣", "♥", "♦"];
-
-function generateSymbol() {
-  let randomSymbol = Math.floor(Math.random() * 4);
-  let randomNumber = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "J",
-    "Q",
-    "K"
-  ];
-  let spans = document.getElementsByTagName("span");
-  spans[1].innerHTML = randomNumber[Math.floor(Math.random() * 12)];
-  spans[0].innerHTML = symbol[randomSymbol];
-  spans[2].innerHTML = symbol[randomSymbol];
-  if (symbol[randomSymbol] == "♥" || symbol[randomSymbol] == "♦") {
-    spans[0].className = "symbol-left-red";
-    spans[2].className = "symbol-right-red";
-    return;
+  function generateSymbol() {
+    let randomSymbol = Math.floor(Math.random() * 4);
+    let randomNumber = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "J",
+      "Q",
+      "K"
+    ];
+    let spans = document.getElementsByTagName("span");
+    spans[1].innerHTML = randomNumber[Math.floor(Math.random() * 12)];
+    spans[0].innerHTML = symbol[randomSymbol];
+    spans[2].innerHTML = symbol[randomSymbol];
+    if (symbol[randomSymbol] == "♥" || symbol[randomSymbol] == "♦") {
+      spans[0].className = "symbol-left-red";
+      spans[2].className = "symbol-right-red";
+      return;
+    }
+    spans[0].className = "symbol-left-black";
+    spans[2].className = "symbol-right-black";
   }
-  spans[0].className = "symbol-left-black";
-  spans[2].className = "symbol-right-black";
-}
+  let buttons = document.getElementsByTagName("button");
+  for (const element of buttons) {
+    element.addEventListener("click", e => {
+      let isBtnGenerator = e.target.className === "btn-generate";
+      if (isBtnGenerator) {
+        generateSymbol();
+        clearInterval(timer);
+      }
+      let setWidth = document.getElementById("input-width").value;
+      let setHeight = document.getElementById("input-height").value;
+      setWidth *= 10;
+      setHeight *= 10;
+      //less size w=350 h=350 max w=1000 h=1000
+      if (
+        setWidth != "" &&
+        setHeight != "" &&
+        setWidth >= 350 &&
+        setHeight >= 350
+      ) {
+        let selectCard = document.getElementById("card");
+        selectCard.style.width = `${setWidth}px`;
+        selectCard.style.height = `${setHeight}px`;
+      } else {
+        alert("input size is not valid");
+      }
+    });
+  }
+};
